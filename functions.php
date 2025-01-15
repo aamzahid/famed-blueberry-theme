a073f72f37b39d30b3ee0bcbe0c1bf069c4b7864
aamzahid <aamzahidt@gmail.com>
2025-01-15 10:55:15 +0600

    header added

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php 
+
+function famed_files(){
+    wp_enqueue_script( 'famed-js', get_theme_file_uri('/build/index.js'), array('jquery'),'1.0', true);
+    wp_enqueue_style( 'custom-google-fonts','https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style( 'font-awesome','https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style( 'famed_main_styles',get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style( 'famed_extra_styles',get_theme_file_uri('/build/index.css'));
+
+}
+add_action('wp_enqueue_scripts','famed_files');
+
+// function famed_features(){
+//     add_theme_support('title-tag');
+// }
+
+// add_action('after_setup_theme', 'famed-features');
